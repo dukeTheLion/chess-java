48eb3c3e08752669e48e6ae6353935233c5bfd68
dukeTheLion <savio_gm@icloud.com>
2020-01-15 14:27:20 -0300

    Implements 17

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -17,12 +17,29 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
+        boolean temp = true;
+
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+        String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+        String ANSI_YELLOW = "\u001B[33m";
+        String ANSI_BLUE = "\u001B[34m";
+
         while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
                 System.out.print(" | ");
-                System.out.print("Source: ");
+                if (temp){
+                    System.out.print(ANSI_BLACK_BACKGROUND + ANSI_BLUE + " Source: " + ANSI_RESET + " ");
+
+                    temp = false;
+                } else {
+                    System.out.print(ANSI_WHITE_BACKGROUND + ANSI_YELLOW + " Source: " + ANSI_RESET + " ");
+
+                    temp = true;
+                }
+
                 ChessPosition source = UI.readChessPosition(sc);
 
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
@@ -30,7 +47,11 @@ public class Program {
 
                 UI.printMatch(chessMatch, possibleMoves, captured);
                 System.out.print(" | ");
-                System.out.print("Target: ");
+                if (!temp){
+                    System.out.print(ANSI_BLACK_BACKGROUND + ANSI_BLUE + " Target: " + ANSI_RESET + " ");
+                } else {
+                    System.out.print(ANSI_WHITE_BACKGROUND + ANSI_YELLOW + " Target: " + ANSI_RESET + " ");
+                }
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
